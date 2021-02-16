d65d120cb802285cb3211c610054394902ba4254
Jason <kangja1996@gmail.com>
2021-02-16 00:43:20 -0500

    progress

--- reverse-a-string.js
+++ reverse-a-string.js
@@ -13,10 +13,12 @@ function reverse(str) {
  
   return backwards.join("");
   // return backwards;
-
-  function reverse2(str) {
-    return str.split("").reverse.join("")
-  }
 }
 
-console.log(reverse("Hi my name is Jason"));
+  function reverse2(str) {
+    return str.split("").reverse().join("")
+  }
+
+  const reverse3 = str => [...str].reverse().join("");
+
+console.log(reverse3("Hi my name is Jason"));
